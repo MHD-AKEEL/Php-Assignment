b4efbcc1e50cd3b66d4ee3145f7ebf254302b0f2
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-21 18:28:54 +0300

    modified bill_form.php

--- 02.Assignment/bill_form.php
+++ 02.Assignment/bill_form.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bill Calculation</title>
 </head>
+
 <body>
     <h2>Grocery Bill Calculation</h2>
-    <form action="">
-        Biscuits (Rs50 each): 
+    <form method="post" action="bill_joint.php">
+        Biscuits (Rs50 each):
         <input type="number" name="biscuits"> <br><br>
-        Noodles (Rs100 each): 
+        Noodles (Rs100 each):
         <input type="number" name="noodles"> <br><br>
-        Bread (Rs40 each): 
+        Bread (Rs40 each):
         <input type="number" name="bread"> <br><br>
-        Milk (Rs60 each): 
+        Milk (Rs60 each):
         <input type="number" name="milk"> <br><br>
-        Eggs (Rs5 each): 
+        Eggs (Rs5 each):
         <input type="number" name="eggs"> <br><br>
-        Dhal (Rs75 each kg): 
+        Dhal (Rs75 each kg):
         <input type="number" name="dhal"> <br><br>
 
         <input type="submit" value="Calculate Bill">
         <input type="reset" value="Clear">
     </form>
 </body>
+
 </html>
