aebffd6381cf434b5b8565b8060682df3f949ca5
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-22 23:30:40 +0300

    modified shop_invoice.php

--- 03.Assignment/shop_invoice.php
+++ 03.Assignment/shop_invoice.php
@@ -15,19 +15,26 @@
             <tr>
                 <td>
                     <div class="form-row">
-                        <label>Shop Name:</label> <input type="text" name="name"><br>
-                    </div>
-                    <div class="form-row">
-                        <label>Address: </label><input type="text" name="address"><br>
-                    </div>
-                    <div class="form-row">
-                        <label>Contact Number: </label><input type="text" name="contact_number"><br>
-                    </div>
-                    <div class="form-row">
-                        <label>Email Address: </label><input type="text" name="email"><br>
+                        <label>Shop Name:</label>
+                        <input type="text" name="name"><br>
                     </div>
 
-                    <table>
+                    <div class="form-row">
+                        <label>Address:</label>
+                        <input type="text" name="address"><br>
+                    </div>
+
+                    <div class="form-row">
+                        <label>Contact Number:</label>
+                        <input type="text" name="contact_number"><br>
+                    </div>
+
+                    <div class="form-row">
+                        <label>Email Address:</label>
+                        <input type="text" name="email"><br>
+                    </div>
+
+                    <table border="0" cellpadding="20" cellspacing="0">
                         <tr>
                             <th>Item Code</th>
                             <th>Item Name</th>
@@ -36,24 +43,24 @@
                         </tr>
 
                         <tr>
-                            <td><input type="text" value="ABC-001"></td>
-                            <td><input type="text" value="Yoghurt"></td>
-                            <td><input type="number"></td>
-                            <td><input type="text" value="400.00"></td>
+                            <td>ABC-001</td>
+                            <td>Yoghurt</td>
+                            <td><input type="number" name="qty_ABC001"></td>
+                            <td>400.00</td>
                         </tr>
 
                         <tr>
-                            <td><input type="text" value="ABC-002"></td>
-                            <td><input type="text" value="Milk"></td>
-                            <td><input type="number"></td>
-                            <td><input type="text" value="1380.00"></td>
+                            <td>ABC-002</td>
+                            <td>Milk</td>
+                            <td><input type="number" name="qty_ABC002"></td>
+                            <td>1380.00</td>
                         </tr>
 
                         <tr>
-                            <td><input type="text" value="ABC-003"></td>
-                            <td><input type="text" value="Butter"></td>
-                            <td><input type="number"></td>
-                            <td><input type="text" value="240.00"></td>
+                            <td>ABC-003</td>
+                            <td>Butter</td>
+                            <td><input type="number" name="qty_ABC003"></td>
+                            <td>240.00</td>
                         </tr>
                     </table><br>
 
